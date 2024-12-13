8165be39bed6f34ead16102cc934e7e73a270874
Goutam Kumar <goutamkumar42010no@gmail.com>
2024-12-13 17:16:25 +0530

    some minor changes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -147,8 +147,9 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true, //cookie cannot accessed by client side js
-      secure: process.env.NODE_ENV === "production",
+      //secure: process.env.NODE_ENV === "production",
       //sameSite: "strict",
+      secure: true,
       sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
       path: "/",
